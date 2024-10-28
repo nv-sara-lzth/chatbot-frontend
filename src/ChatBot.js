e9c800667355ebce9926e361d2a024d5ce7a3391
Sara Ipiales Delgado <>
2024-10-28 21:44:47 +0100

    Update ruta chatbot

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -14,7 +14,7 @@ export default function ChatBot() {
         setUserInput("");
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/chat", {
+            const response = await fetch("http://127.0.0.1:5000/api/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message: userInput })
